23acea49eaf388d546b2cbd0faa0a01b4c96849d
HoangCode <you@example.com>
2023-01-10 09:34:32 +0700

    update header

--- Header/header_da_1_0_0/js/header_da_1_0_0.js
+++ Header/header_da_1_0_0/js/header_da_1_0_0.js
@@ -1,10 +1,10 @@
 document.getElementById("headerMenuBtn").addEventListener("click", () => {
-    document.getElementById("headerSideBar").classList.add("show");
-    document.getElementById("headerBg").style.display = "block";
+    document.getElementById("headerSideBar").classList.toggle("show");
+    document.getElementById("headerBg").classList.toggle("active");
 });
 document.getElementById("headerBg").addEventListener("click", () => {
     document.getElementById("headerSideBar").classList.remove("show");
-    document.getElementById("headerBg").style.display = "none";
+    document.getElementById("headerBg").classList.remove("active");
 });
 
 const menuItem = document.querySelectorAll(".header_da_1_0_0__item");
