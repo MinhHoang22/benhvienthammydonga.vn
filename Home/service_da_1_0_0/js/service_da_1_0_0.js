0be68540a3ac21fa52efd1cc2014fd8a4015e67b
HoangCode <you@example.com>
2023-02-01 16:33:42 +0700

    update

--- Home/service_da_1_0_0/js/service_da_1_0_0.js
+++ Home/service_da_1_0_0/js/service_da_1_0_0.js
@@ -3,54 +3,54 @@ const dataService = [
         name: "Nâng mũi cấu trúc Nano 1",
         image: "images/cateService_da_1_0_0-2.jpg",
         link: "https://benhvienthammydonga.vn/tham-my-mui/nang-mui-cau-truc/?new",
-        show: true,
+        show: "true",
     },
     {
         name: "Nâng mũi cấu trúc Nano 2",
         image: "images/cateService_da_1_0_0-2.jpg",
         link: "https://benhvienthammydonga.vn/tham-my-mui/nang-mui-cau-truc/?new",
-        show: false,
+        show: "false",
     },
     {
         name: "Nâng mũi cấu trúc Nano 3",
         image: "images/cateService_da_1_0_0-2.jpg",
         link: "https://benhvienthammydonga.vn/tham-my-mui/nang-mui-cau-truc/?new",
-        show: true,
+        show: "true",
     },
     {
         name: "Nâng mũi cấu trúc Nano 4",
         image: "images/cateService_da_1_0_0-2.jpg",
         link: "https://benhvienthammydonga.vn/tham-my-mui/nang-mui-cau-truc/?new",
-        show: true,
+        show: "true",
     },
     {
         name: "Nâng mũi cấu trúc Nano 5",
         image: "images/cateService_da_1_0_0-2.jpg",
         link: "https://benhvienthammydonga.vn/tham-my-mui/nang-mui-cau-truc/?new",
-        show: false,
+        show: "false",
     },
     {
         name: "Nâng mũi cấu trúc Nano 6",
         image: "images/cateService_da_1_0_0-2.jpg",
         link: "https://benhvienthammydonga.vn/tham-my-mui/nang-mui-cau-truc/?new",
-        show: true,
+        show: "true",
     },
     {
         name: "Nâng mũi cấu trúc Nano 7",
         image: "images/cateService_da_1_0_0-2.jpg",
         link: "https://benhvienthammydonga.vn/tham-my-mui/nang-mui-cau-truc/?new",
-        show: true,
+        show: "true",
     },
     {
         name: "Nâng mũi cấu trúc Nano 8",
         image: "images/cateService_da_1_0_0-2.jpg",
         link: "https://benhvienthammydonga.vn/tham-my-mui/nang-mui-cau-truc/?new",
-        show: true,
+        show: "true",
     },
 ];
 
 const dataShow = dataService.filter(item => {
-    return item.show === true;
+    return item.show === "true";
 });
 
 const renderData = (obj) => {
