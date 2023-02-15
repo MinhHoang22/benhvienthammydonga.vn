5631d3801cf15661420d2399f2de1cc6a3cc75c1
nguyenquocviet1002 <vietnguyen10022001@gmail.com>
2023-02-15 17:03:50 +0700

    fix

---
+++ Category/cateNew_da_1_0_0/js/cateNew_da_1_0_0.js
@@ -0,0 +1,248 @@
+const dataNew = [
+  {
+    name: "Sụn surgiform có tốt không? Giá bao nhiêu tiền? Nên lưu ý gì?",
+    link: "https://benhvienthammydonga.vn/tham-my-mui/sun-surgiform-co-tot-khong/",
+    img: "https://benhvienthammydonga.vn/wp-content/uploads/2023/02/sun-surgiform-co-tot-khong-300x210.jpg",
+    excerpt:
+      "Sụn surgiform được ứng dụng trong việc tạo nên dáng mũi cao thẳng, được nhiều chuyên gia thẩm mỹ đánh giá là một trong những chất liệu nâng mũi tốt nhất hiện nay, sở hữu nhiều ưu điểm vượt trội. Thế nhưng vẫn còn khá nhiều băn khoăn sụn",
+    cate: "Thẩm mỹ mũi",
+    cateGroup: "Phẫu thuật thẩm mỹ",
+  },
+  {
+    name: "Sụn surgiform của nước nào? Giá bao nhiêu? Có tốt không?",
+    link: "https://benhvienthammydonga.vn/tham-my-mui/sun-surgiform-cua-nuoc-nao/",
+    img: "https://benhvienthammydonga.vn/wp-content/uploads/2023/02/sun-surgiform-300x210.jpg",
+    excerpt:
+      "Sụn surgiform là sụn sinh học học được dùng để thay thế cho các loại sụn silicon truyền thống trong phẫu thuật nâng mũi. Với những ưu điểm vượt trội, sụn surgiform luôn là sự lựa chọn hàng đầu. Nếu bạn đang quan tâm sụn surgiform của nước nào?",
+    cate: "Thẩm mỹ mũi",
+    cateGroup: "Phẫu thuật thẩm mỹ",
+  },
+  {
+    name: "Nâng mũi bọc sụn cho nam giới đẹp chuẩn &#8220;soái ca&#8221;",
+    link: "https://benhvienthammydonga.vn/tham-my-mui/nang-mui-boc-sun-cho-nam-gioi/",
+    img: "https://benhvienthammydonga.vn/wp-content/uploads/2023/02/nang-mui-boc-sun-cho-nam-300x210.jpg",
+    excerpt:
+      "Làm đẹp ngày nay không chỉ có riêng chị em phụ nữ mà đấng mày râu cũng nhu cầu làm đẹp ngày một đi lên đặc biệt là nâng mũi. Phương pháp được nhiều chàng lựa chọn là nâng mũi bọc sụn cho nam giới. Vậy phương pháp này",
+    cate: "Thẩm mỹ mũi",
+    cateGroup: "Phẫu thuật thẩm mỹ",
+  },
+  {
+    name: "Bác sĩ nâng mũi bọc sụn giỏi được nhiều người lựa chọn",
+    link: "https://benhvienthammydonga.vn/tham-my-mui/bac-si-nang-mui-boc-sun-gioi/",
+    img: "https://benhvienthammydonga.vn/wp-content/uploads/2023/02/bac-si-nang-mui-boc-sun-300x210.jpg",
+    excerpt:
+      "Nâng mũi bọc sụn là một trong những kỹ thuật nâng mũi phức tạp đòi hỏi tay nghề cũng như trình độ của bác sĩ. Do đó, việc lựa chọn bác sĩ nâng mũi bọc sụn giỏi quyết định tới 90% sự thành công của ca phẫu thuật, đem",
+    cate: "Thẩm mỹ mũi",
+    cateGroup: "Phẫu thuật thẩm mỹ",
+  },
+  {
+    name: "Sụn nâng mũi Hàn Quốc có tốt không? Công nghệ phù hợp nhất?",
+    link: "https://benhvienthammydonga.vn/tham-my-mui/sun-mui-han-quoc/",
+    img: "https://benhvienthammydonga.vn/wp-content/uploads/2023/02/SUN-MUI-HAN-QUOC-300x210.jpg",
+    excerpt:
+      "Sụn mũi Hàn Quốc là một trong những loại sụn được sử rộng phổ biến trong phẫu thuật thẩm mỹ khi nâng mũi công nghệ Hàn Quốc. Sụn nâng mũi Hàn Quốc có sụn gì? Có những loại nào? Mời bạn đọc cùng tìm hiểu qua bài viết sau",
+    cate: "Thẩm mỹ mũi",
+    cateGroup: "Phẫu thuật thẩm mỹ",
+  },
+  {
+    name: "Sụn nâng mũi Hàn Quốc có tốt không? Công nghệ phù hợp nhất 123?",
+    link: "https://benhvienthammydonga.vn/tham-my-mui/sun-mui-han-quoc/",
+    img: "https://benhvienthammydonga.vn/wp-content/uploads/2023/02/SUN-MUI-HAN-QUOC-300x210.jpg",
+    excerpt:
+      "Sụn mũi Hàn Quốc là một trong những loại sụn được sử rộng phổ biến trong phẫu thuật thẩm mỹ khi nâng mũi công nghệ Hàn Quốc. Sụn nâng mũi Hàn Quốc có sụn gì? Có những loại nào? Mời bạn đọc cùng tìm hiểu qua bài viết sau",
+    cate: "Thẩm mỹ mũi",
+    cateGroup: "Phẫu thuật thẩm mỹ",
+  },
+  {
+    name: "Cấy tóc có hiệu quả không? Duy trì được bao lâu? Có vĩnh viễn không?",
+    link: "https://benhvienthammydonga.vn/cay-toc/cay-toc-co-hieu-qua-khong/",
+    img: "https://benhvienthammydonga.vn/wp-content/uploads/2022/03/cay-toc-co-hieu-qua-khong-thump-300x210.jpg",
+    excerpt:
+      "Cấy tóc là giải pháp an toàn cho sức khỏe phù hợp với người rụng tóc, hói lâu năm. Vậy cấy tóc có hiệu quả không là thắc mắc của nhiều khách hàng đang gặp phải thực trạng trên. Để giúp khách hàng có góc nhìn khách quan, chi",
+    cate: "Cấy tóc",
+    cateGroup: "Tin tức làm đẹp",
+  },
+  {
+    name: "Cấy tóc có Đau không? Giải đáp từ chuyên gia về hiệu quả",
+    link: "https://benhvienthammydonga.vn/cay-toc/cay-toc-co-dau-khong/",
+    img: "https://benhvienthammydonga.vn/wp-content/uploads/2020/11/phau-thuat-cay-toc-dau-khong-1-300x210.jpg",
+    excerpt:
+      "Hình ảnh bác sĩ bóc tách từng nang tóc đã làm nhiều người hoang mang tự hỏi cấy tóc có đau không? Tính đến thời điểm hiện nay, cấy tóc là kỹ thuật hiệu quả giúp điều trị các vấn đề tóc hói, đường chân tóc quá cao...thu hút",
+    cate: "Cấy tóc",
+    cateGroup: "Tin tức làm đẹp",
+  },
+  {
+    name: "Cấy tóc sinh học là gì? Giữ được bao lâu? Giá bao nhiêu tiền?",
+    link: "https://benhvienthammydonga.vn/cay-toc/cay-toc-sinh-hoc/",
+    img: "https://benhvienthammydonga.vn/wp-content/uploads/2020/12/cay-toc-sinh-hoc-thump-300x210.jpg",
+    excerpt:
+      "Công nghệ cấy tóc sinh học kể từ khi ra đời đã gây nên tiếng vang lớn trong cộng đồng các tín đồ thẩm mỹ trên khắp thế giới. Đây chính là giải pháp hoàn hảo cho những người bị hói bẩm sinh, những người đang phải đấu tranh",
+    cate: "Cấy tóc",
+    cateGroup: "Tin tức làm đẹp",
+  },
+  {
+    name: "Cấy tóc có nguy hiểm không? Độ hiệu quả và tác dụng phụ có thể xảy ra",
+    link: "https://benhvienthammydonga.vn/cay-toc/cay-toc-co-nguy-hiem-khong/",
+    img: "https://benhvienthammydonga.vn/wp-content/uploads/2020/08/cay-toc-co-nguy-hiem-khong-thump-300x210.jpg",
+    excerpt:
+      "Cấy tóc có nguy hiểm không luôn là câu hỏi khiến nhiều người lo lắng trước khi có ý định thực hiện. Vậy trên thực thế, phương pháp trị hói đầu, rụng tóc này có thực sự gây nguy hiểm hay làm hại cho da đầu của bạn? Cùng",
+    cate: "Cấy tóc",
+    cateGroup: "Tin tức làm đẹp",
+  },
+  {
+    name: "Cấy tóc ở đâu Đẹp &#8211; Uy Tín &#8211; Tốt Nhất tại Hà Nội và Tp. Hcm",
+    link: "https://benhvienthammydonga.vn/cay-toc/cay-toc-o-dau-tot-nhat/",
+    img: "https://benhvienthammydonga.vn/wp-content/uploads/2020/08/cay-toc-o-dau-tot-nhat-thump-300x210.jpg",
+    excerpt:
+      "Cấy tóc ở đâu tốt nhất là vấn đề mà rất nhiều bị thưa tóc, hói đầu quan tâm. Vậy một cơ sở cấy tóc dược đánh giá là uy tín, chất lượng cần phải đáp ứng được những tiêu chí nào? Cùng tìm hiểu ngay tại bài viết",
+    cate: "Cấy tóc",
+    cateGroup: "Tin tức làm đẹp",
+  },
+  {
+    name: "Nguyên nhân hình thành nám da",
+    link: "https://benhvienthammydonga.vn/dieu-tri-nam/nguyen-nhan-hinh-thanh-nam/",
+    img: "https://benhvienthammydonga.vn/wp-content/uploads/2023/01/nguyen-nhan-hinh-thanh-nam-300x210.jpg",
+    excerpt:
+      "Nám da là hiện tượng rối loạn sắc tố thường gặp ở phụ nữ sau 30 tuổi, gây ảnh hưởng đến thẩm mỹ khiến nhiều người mất tự tin. Vậy nguyên nhân hình thành nám da là gì, làm sao để phòng ngừa và điều trị da bị nám",
+    cate: "Điều trị nám",
+    cateGroup: "Tin tức làm đẹp",
+  },
+  {
+    name: "Tổng hợp 11 cách trị nám từ bên trong đơn giản tại nhà",
+    link: "https://benhvienthammydonga.vn/dieu-tri-nam/cach-tri-nam-tu-ben-trong/",
+    img: "https://benhvienthammydonga.vn/wp-content/uploads/2023/01/cach-tri-nam-tu-ben-trong-300x210.jpg",
+    excerpt:
+      "Muốn trị nám da không tái phát, chỉ tác động từ bên ngoài là chưa đủ, cần phải kết hợp với chế độ ăn uống phù hợp để điều chỉnh từ bên trong. Cùng tìm hiểu 11 cách trị nám từ bên trong an toàn, hiệu quả dễ thực",
+    cate: "Điều trị nám",
+    cateGroup: "Tin tức làm đẹp",
+  },
+  {
+    name: "Nhận biết dấu hiệu bị nám da mặt",
+    link: "https://benhvienthammydonga.vn/dieu-tri-nam/dau-hieu-bi-nam-da-mat/",
+    img: "https://benhvienthammydonga.vn/wp-content/uploads/2023/01/dau-hieu-bi-nam-da-mat-300x210.jpg",
+    excerpt:
+      "Trước khi hình thành những đốm, mảng nám trên mặt, da sẽ có những cảnh báo nám tiềm ẩn. Nhận biết được những dấu hiệu bị nám da mặt, giúp sớm phát hiện tình trạng da tăng sắc tố. Từ đó điều trị đơn giản và hiệu quả hơn,",
+    cate: "Điều trị nám",
+    cateGroup: "Tin tức làm đẹp",
+  },
+  {
+    name: "Hình ảnh nám da mặt &#8211; Cách nhận biết nám da",
+    link: "https://benhvienthammydonga.vn/dieu-tri-nam/hinh-anh-nam-da-mat/",
+    img: "https://benhvienthammydonga.vn/wp-content/uploads/2023/01/hinh-anh-nam-da-mat-300x210.jpg",
+    excerpt:
+      "Bạn lo lắng những nốt sậm màu xuất hiện trên má liệu có phải da bị nám? Nếu chưa biết cách nhận biết, tham khảo ngay hình ảnh nám da mặt để đối chiếu với tình trạng hiện tại của bản thân.[/sapo] I. Hình ảnh da mặt bị",
+    cate: "Điều trị nám",
+    cateGroup: "Tin tức làm đẹp",
+  },
+  {
+    name: "Có nên điều trị nám bằng tia Laser không? Giá tiền bao nhiêu?",
+    link: "https://benhvienthammydonga.vn/dieu-tri-nam/dieu-tri-nam-bang-laser/",
+    img: "https://benhvienthammydonga.vn/wp-content/uploads/2022/12/tri-nam-bang-tia-New-E-light-hien-dai-1-300x210.jpg",
+    excerpt:
+      "Điều trị nám bằng Laser là kỹ thuật được ứng dụng rộng rãi trên thị trường. Sức mạnh vượt trội của tia laser đánh bay gốc nám cứng đầu đem lại làn da sáng bóng, mịn màng. Quá trình đốt nám bằng laser có thể kết hợp các liệu",
+    cate: "Điều trị nám",
+    cateGroup: "Tin tức làm đẹp",
+  },
+];
+
+const categories = [];
+dataNew.map((item) => {
+  categories.push(item.cateGroup);
+});
+const category = categories.filter((item, index) => {
+  return categories.indexOf(item) === index;
+});
+
+const sortData = (obj) => {
+  let categories = [];
+  obj.map((item) => {
+    categories.push(item.cate);
+  });
+  let category = categories.filter((item, index) => {
+    return categories.indexOf(item) === index;
+  });
+  return category;
+};
+
+const renderTabs = (arr) => {
+  let html = "";
+  for (item of arr) {
+    html += `<div class="doctor_da_2_0_0__tablink" data-tab="${item}" onclick="renderData(this)">${item}</div>`;
+  }
+  return html;
+};
+
+document.getElementById("tab").innerHTML = renderTabs(category);
+
+const renderData = (ele) => {
+  const elements = document.querySelectorAll(".doctor_da_2_0_0__tablink");
+  elements.forEach(function (element) {
+    element.classList.remove("current");
+  });
+  ele.classList.add("current");
+  const dataID = ele.getAttribute("data-tab");
+  let a = renderList(dataNew, dataID);
+  let da = renderTow(a);
+  renderThree(da);
+};
+
+const renderList = (arr, filter) => {
+  let dataTab = arr.filter((item) => {
+    return item.cateGroup === filter;
+  });
+  return dataTab;
+};
+
+const renderTow = (obj) => {
+  let d = [];
+  let b = renderList(obj, obj[0].cateGroup);
+  let c = sortData(b);
+  c.map((item2) => {
+    let x = b.filter((item) => {
+      return item.cate === item2;
+    });
+    d.push(x);
+  });
+  return d;
+};
+
+const renderFour = (obj) => {
+    let newobj2 = obj.slice(0, 5);
+    console.log(obj)
+    let htmlBig = `
+        <a href="#" class="cateNew_da_1_0_0__content--big">${obj[0].name}</a>
+    `;
+    let html = '';
+    let html2 = '';
+    
+    let newObj = obj;
+    newObj.shift();
+   console.log("new",newObj)
+
+    for (let i = 0; i < newObj.length; i++){
+        html += `
+        <a href="#" class="cateNew_da_1_0_0__content--small">${obj[i].name}</a>
+        `
+    };
+    html2 = `
+    <div class="row">
+    <h3>${obj[0].cate}</h3>
+    <div>
+        ${htmlBig}
+        ${html}
+    </div>
+    </div>
+    `
+    // console.log(html2)
+    return html2;
+}
+
+const renderThree = (obj) => {
+    let html = ""
+    let l = obj.length;
+    for(let i = 0; i < l; i++){
+        html += `${renderFour(obj[i])}`
+    }
+    console.log(html);
+}
+
