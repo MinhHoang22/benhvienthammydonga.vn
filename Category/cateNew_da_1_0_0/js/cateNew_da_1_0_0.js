f8063d7040c7399f6679fab74afcb7ca888b4a1e
nguyenquocviet1002 <vietnguyen10022001@gmail.com>
2023-02-16 10:53:12 +0700

    update

--- Category/cateNew_da_1_0_0/js/cateNew_da_1_0_0.js
+++ Category/cateNew_da_1_0_0/js/cateNew_da_1_0_0.js
@@ -1,5 +1,25 @@
 const dataNew = [
   {
+    name: "Sụn silicon và surgiform là gì? Nâng mũi loại nào tốt nhất?",
+    link: "https://benhvienthammydonga.vn/tham-my-mui/sun-silicon-va-surgiform/",
+    img: "https://benhvienthammydonga.vn/wp-content/uploads/2023/02/sun-silicon-300x210.jpg",
+    excerpt:
+      "Thẩm mỹ mũi đang là xu hướng của phụ nữ hiện nay. Cùng với đó là sự thay đổi về phương pháp và chất liệu sụn. Trong đó nâng mũi sụn silicon và surgiform đang rất được ưa chuộng trên thị trường. Hai loại sụn này có đặc điểm",
+    cate: "Thẩm mỹ mũi",
+    cateGroup: "Điều trị - Trẻ hóa da",
+    date: "15 Tháng Hai, 2023",
+  },
+  {
+    name: "Sụn silicon và surgiform là gì? Nâng mũi loại nào tốt nhất?",
+    link: "https://benhvienthammydonga.vn/tham-my-mui/sun-silicon-va-surgiform/",
+    img: "https://benhvienthammydonga.vn/wp-content/uploads/2023/02/sun-silicon-300x210.jpg",
+    excerpt:
+      "Thẩm mỹ mũi đang là xu hướng của phụ nữ hiện nay. Cùng với đó là sự thay đổi về phương pháp và chất liệu sụn. Trong đó nâng mũi sụn silicon và surgiform đang rất được ưa chuộng trên thị trường. Hai loại sụn này có đặc điểm",
+    cate: "Thẩm mỹ mũi",
+    cateGroup: "Phẫu thuật thẩm mỹ",
+    date: "15 Tháng Hai, 2023",
+  },
+  {
     name: "Sụn surgiform có tốt không? Giá bao nhiêu tiền? Nên lưu ý gì?",
     link: "https://benhvienthammydonga.vn/tham-my-mui/sun-surgiform-co-tot-khong/",
     img: "https://benhvienthammydonga.vn/wp-content/uploads/2023/02/sun-surgiform-co-tot-khong-300x210.jpg",
@@ -7,6 +27,7 @@ const dataNew = [
       "Sụn surgiform được ứng dụng trong việc tạo nên dáng mũi cao thẳng, được nhiều chuyên gia thẩm mỹ đánh giá là một trong những chất liệu nâng mũi tốt nhất hiện nay, sở hữu nhiều ưu điểm vượt trội. Thế nhưng vẫn còn khá nhiều băn khoăn sụn",
     cate: "Thẩm mỹ mũi",
     cateGroup: "Phẫu thuật thẩm mỹ",
+    date: "15 Tháng Hai, 2023",
   },
   {
     name: "Sụn surgiform của nước nào? Giá bao nhiêu? Có tốt không?",
@@ -16,6 +37,7 @@ const dataNew = [
       "Sụn surgiform là sụn sinh học học được dùng để thay thế cho các loại sụn silicon truyền thống trong phẫu thuật nâng mũi. Với những ưu điểm vượt trội, sụn surgiform luôn là sự lựa chọn hàng đầu. Nếu bạn đang quan tâm sụn surgiform của nước nào?",
     cate: "Thẩm mỹ mũi",
     cateGroup: "Phẫu thuật thẩm mỹ",
+    date: "15 Tháng Hai, 2023",
   },
   {
     name: "Nâng mũi bọc sụn cho nam giới đẹp chuẩn &#8220;soái ca&#8221;",
@@ -25,6 +47,7 @@ const dataNew = [
       "Làm đẹp ngày nay không chỉ có riêng chị em phụ nữ mà đấng mày râu cũng nhu cầu làm đẹp ngày một đi lên đặc biệt là nâng mũi. Phương pháp được nhiều chàng lựa chọn là nâng mũi bọc sụn cho nam giới. Vậy phương pháp này",
     cate: "Thẩm mỹ mũi",
     cateGroup: "Phẫu thuật thẩm mỹ",
+    date: "15 Tháng Hai, 2023",
   },
   {
     name: "Bác sĩ nâng mũi bọc sụn giỏi được nhiều người lựa chọn",
@@ -34,24 +57,7 @@ const dataNew = [
       "Nâng mũi bọc sụn là một trong những kỹ thuật nâng mũi phức tạp đòi hỏi tay nghề cũng như trình độ của bác sĩ. Do đó, việc lựa chọn bác sĩ nâng mũi bọc sụn giỏi quyết định tới 90% sự thành công của ca phẫu thuật, đem",
     cate: "Thẩm mỹ mũi",
     cateGroup: "Phẫu thuật thẩm mỹ",
-  },
-  {
-    name: "Sụn nâng mũi Hàn Quốc có tốt không? Công nghệ phù hợp nhất?",
-    link: "https://benhvienthammydonga.vn/tham-my-mui/sun-mui-han-quoc/",
-    img: "https://benhvienthammydonga.vn/wp-content/uploads/2023/02/SUN-MUI-HAN-QUOC-300x210.jpg",
-    excerpt:
-      "Sụn mũi Hàn Quốc là một trong những loại sụn được sử rộng phổ biến trong phẫu thuật thẩm mỹ khi nâng mũi công nghệ Hàn Quốc. Sụn nâng mũi Hàn Quốc có sụn gì? Có những loại nào? Mời bạn đọc cùng tìm hiểu qua bài viết sau",
-    cate: "Thẩm mỹ mũi",
-    cateGroup: "Phẫu thuật thẩm mỹ",
-  },
-  {
-    name: "Sụn nâng mũi Hàn Quốc có tốt không? Công nghệ phù hợp nhất 123?",
-    link: "https://benhvienthammydonga.vn/tham-my-mui/sun-mui-han-quoc/",
-    img: "https://benhvienthammydonga.vn/wp-content/uploads/2023/02/SUN-MUI-HAN-QUOC-300x210.jpg",
-    excerpt:
-      "Sụn mũi Hàn Quốc là một trong những loại sụn được sử rộng phổ biến trong phẫu thuật thẩm mỹ khi nâng mũi công nghệ Hàn Quốc. Sụn nâng mũi Hàn Quốc có sụn gì? Có những loại nào? Mời bạn đọc cùng tìm hiểu qua bài viết sau",
-    cate: "Thẩm mỹ mũi",
-    cateGroup: "Phẫu thuật thẩm mỹ",
+    date: "14 Tháng Hai, 2023",
   },
   {
     name: "Cấy tóc có hiệu quả không? Duy trì được bao lâu? Có vĩnh viễn không?",
@@ -61,6 +67,7 @@ const dataNew = [
       "Cấy tóc là giải pháp an toàn cho sức khỏe phù hợp với người rụng tóc, hói lâu năm. Vậy cấy tóc có hiệu quả không là thắc mắc của nhiều khách hàng đang gặp phải thực trạng trên. Để giúp khách hàng có góc nhìn khách quan, chi",
     cate: "Cấy tóc",
     cateGroup: "Tin tức làm đẹp",
+    date: "4 Tháng Tám, 2022",
   },
   {
     name: "Cấy tóc có Đau không? Giải đáp từ chuyên gia về hiệu quả",
@@ -70,6 +77,7 @@ const dataNew = [
       "Hình ảnh bác sĩ bóc tách từng nang tóc đã làm nhiều người hoang mang tự hỏi cấy tóc có đau không? Tính đến thời điểm hiện nay, cấy tóc là kỹ thuật hiệu quả giúp điều trị các vấn đề tóc hói, đường chân tóc quá cao...thu hút",
     cate: "Cấy tóc",
     cateGroup: "Tin tức làm đẹp",
+    date: "4 Tháng Tám, 2022",
   },
   {
     name: "Cấy tóc sinh học là gì? Giữ được bao lâu? Giá bao nhiêu tiền?",
@@ -79,6 +87,7 @@ const dataNew = [
       "Công nghệ cấy tóc sinh học kể từ khi ra đời đã gây nên tiếng vang lớn trong cộng đồng các tín đồ thẩm mỹ trên khắp thế giới. Đây chính là giải pháp hoàn hảo cho những người bị hói bẩm sinh, những người đang phải đấu tranh",
     cate: "Cấy tóc",
     cateGroup: "Tin tức làm đẹp",
+    date: "4 Tháng Tám, 2022",
   },
   {
     name: "Cấy tóc có nguy hiểm không? Độ hiệu quả và tác dụng phụ có thể xảy ra",
@@ -88,6 +97,7 @@ const dataNew = [
       "Cấy tóc có nguy hiểm không luôn là câu hỏi khiến nhiều người lo lắng trước khi có ý định thực hiện. Vậy trên thực thế, phương pháp trị hói đầu, rụng tóc này có thực sự gây nguy hiểm hay làm hại cho da đầu của bạn? Cùng",
     cate: "Cấy tóc",
     cateGroup: "Tin tức làm đẹp",
+    date: "4 Tháng Tám, 2022",
   },
   {
     name: "Cấy tóc ở đâu Đẹp &#8211; Uy Tín &#8211; Tốt Nhất tại Hà Nội và Tp. Hcm",
@@ -97,6 +107,7 @@ const dataNew = [
       "Cấy tóc ở đâu tốt nhất là vấn đề mà rất nhiều bị thưa tóc, hói đầu quan tâm. Vậy một cơ sở cấy tóc dược đánh giá là uy tín, chất lượng cần phải đáp ứng được những tiêu chí nào? Cùng tìm hiểu ngay tại bài viết",
     cate: "Cấy tóc",
     cateGroup: "Tin tức làm đẹp",
+    date: "4 Tháng Tám, 2022",
   },
   {
     name: "Nguyên nhân hình thành nám da",
@@ -106,6 +117,7 @@ const dataNew = [
       "Nám da là hiện tượng rối loạn sắc tố thường gặp ở phụ nữ sau 30 tuổi, gây ảnh hưởng đến thẩm mỹ khiến nhiều người mất tự tin. Vậy nguyên nhân hình thành nám da là gì, làm sao để phòng ngừa và điều trị da bị nám",
     cate: "Điều trị nám",
     cateGroup: "Tin tức làm đẹp",
+    date: "21 Tháng Một, 2023",
   },
   {
     name: "Tổng hợp 11 cách trị nám từ bên trong đơn giản tại nhà",
@@ -115,6 +127,7 @@ const dataNew = [
       "Muốn trị nám da không tái phát, chỉ tác động từ bên ngoài là chưa đủ, cần phải kết hợp với chế độ ăn uống phù hợp để điều chỉnh từ bên trong. Cùng tìm hiểu 11 cách trị nám từ bên trong an toàn, hiệu quả dễ thực",
     cate: "Điều trị nám",
     cateGroup: "Tin tức làm đẹp",
+    date: "16 Tháng Một, 2023",
   },
   {
     name: "Nhận biết dấu hiệu bị nám da mặt",
@@ -124,6 +137,7 @@ const dataNew = [
       "Trước khi hình thành những đốm, mảng nám trên mặt, da sẽ có những cảnh báo nám tiềm ẩn. Nhận biết được những dấu hiệu bị nám da mặt, giúp sớm phát hiện tình trạng da tăng sắc tố. Từ đó điều trị đơn giản và hiệu quả hơn,",
     cate: "Điều trị nám",
     cateGroup: "Tin tức làm đẹp",
+    date: "13 Tháng Một, 2023",
   },
   {
     name: "Hình ảnh nám da mặt &#8211; Cách nhận biết nám da",
@@ -133,6 +147,7 @@ const dataNew = [
       "Bạn lo lắng những nốt sậm màu xuất hiện trên má liệu có phải da bị nám? Nếu chưa biết cách nhận biết, tham khảo ngay hình ảnh nám da mặt để đối chiếu với tình trạng hiện tại của bản thân.[/sapo] I. Hình ảnh da mặt bị",
     cate: "Điều trị nám",
     cateGroup: "Tin tức làm đẹp",
+    date: "12 Tháng Một, 2023",
   },
   {
     name: "Có nên điều trị nám bằng tia Laser không? Giá tiền bao nhiêu?",
@@ -142,6 +157,7 @@ const dataNew = [
       "Điều trị nám bằng Laser là kỹ thuật được ứng dụng rộng rãi trên thị trường. Sức mạnh vượt trội của tia laser đánh bay gốc nám cứng đầu đem lại làn da sáng bóng, mịn màng. Quá trình đốt nám bằng laser có thể kết hợp các liệu",
     cate: "Điều trị nám",
     cateGroup: "Tin tức làm đẹp",
+    date: "29 Tháng Mười Hai, 2022",
   },
 ];
 
@@ -154,20 +170,20 @@ const category = categories.filter((item, index) => {
 });
 
 const sortData = (obj) => {
-  let categories = [];
+  let categoriesSort = [];
   obj.map((item) => {
-    categories.push(item.cate);
+    categoriesSort.push(item.cate);
   });
-  let category = categories.filter((item, index) => {
-    return categories.indexOf(item) === index;
+  let categorySort = categoriesSort.filter((item, index) => {
+    return categoriesSort.indexOf(item) === index;
   });
-  return category;
+  return categorySort;
 };
 
 const renderTabs = (arr) => {
   let html = "";
   for (item of arr) {
-    html += `<div class="doctor_da_2_0_0__tablink" data-tab="${item}" onclick="renderData(this)">${item}</div>`;
+    html += `<div class="tabNew_da_1_0_0__tablink" data-tab="${item}" onclick="renderData(this)">${item}</div>`;
   }
   return html;
 };
@@ -175,15 +191,15 @@ const renderTabs = (arr) => {
 document.getElementById("tab").innerHTML = renderTabs(category);
 
 const renderData = (ele) => {
-  const elements = document.querySelectorAll(".doctor_da_2_0_0__tablink");
+  const elements = document.querySelectorAll(".tabNew_da_1_0_0__tablink");
   elements.forEach(function (element) {
     element.classList.remove("current");
   });
   ele.classList.add("current");
   const dataID = ele.getAttribute("data-tab");
-  let a = renderList(dataNew, dataID);
-  let da = renderTow(a);
-  renderThree(da);
+  let lNew = renderList(dataNew, dataID);
+  let lCateGroup = listCateGroup(lNew);
+  renderContentTab(lCateGroup);
 };
 
 const renderList = (arr, filter) => {
@@ -193,56 +209,80 @@ const renderList = (arr, filter) => {
   return dataTab;
 };
 
-const renderTow = (obj) => {
-  let d = [];
-  let b = renderList(obj, obj[0].cateGroup);
-  let c = sortData(b);
-  c.map((item2) => {
-    let x = b.filter((item) => {
+const listCateGroup = (obj) => {
+  let listCateG = [];
+  let newsCateG = renderList(obj, obj[0].cateGroup);
+  let listCateName = sortData(newsCateG);
+  listCateName.map((item2) => {
+    let itemPush = newsCateG.filter((item) => {
       return item.cate === item2;
     });
-    d.push(x);
+    listCateG.push(itemPush);
   });
-  return d;
+  return listCateG;
 };
 
-const renderFour = (obj) => {
-    let newobj2 = obj.slice(0, 5);
-    console.log(obj)
-    let htmlBig = `
-        <a href="#" class="cateNew_da_1_0_0__content--big">${obj[0].name}</a>
+const renderListNews = (obj) => {
+  let htmlSide = "";
+  let objLimit = obj.slice(0, 5);
+  let objLenght = objLimit.length;
+
+  let htmlMain = `
+        <a href="${objLimit[0].link}" class="cateNew_da_1_0_0__content">
+            <div class="cateNew_da_1_0_0__video">
+                <img width="390" height="230" src="${objLimit[0].img}" alt="${objLimit[0].name}">
+            </div>
+            <div class="cateNew_da_1_0_0__wrap">
+                <h3>${objLimit[0].name}</h3>
+                <p>${objLimit[0].excerpt}</p>
+                <div class="cateNew_da_1_0_0__numb">
+                    <img width="15" height="15" src="https://benhvienthammydonga.vn/wp-content/themes/SCI_Theme/Module/Home/cateNew_da_1_0_0/images/icon.png" alt=""> ${objLimit[0].date}
+                </div>
+            </div>
+        </a>
     `;
-    let html = '';
-    let html2 = '';
-    
-    let newObj = obj;
-    newObj.shift();
-   console.log("new",newObj)
 
-    for (let i = 0; i < newObj.length; i++){
-        html += `
-        <a href="#" class="cateNew_da_1_0_0__content--small">${obj[i].name}</a>
-        `
-    };
-    html2 = `
-    <div class="row">
-    <h3>${obj[0].cate}</h3>
-    <div>
-        ${htmlBig}
-        ${html}
+  for (let i = 1; i < objLenght; i++) {
+    htmlSide += `
+        <a href="${objLimit[i].link}" class="cateNew_da_1_0_0__item">
+            <div class="cateNew_da_1_0_0__pic">
+                <img width="120" height="90" src="${objLimit[i].img}" alt="${objLimit[i].name}">
+            </div>
+            <div class="cateNew_da_1_0_0__detail">
+                <h3>${objLimit[i].name}</h3>
+                <div class="cateNew_da_1_0_0__numb">
+                    <img width="15" height="15" src="https://benhvienthammydonga.vn/wp-content/themes/SCI_Theme/Module/Home/cateNew_da_1_0_0/images/icon.png" alt=""> 08/12/2022
+                </div>
+            </div>
+        </a>
+        `;
+  }
+
+  let htmlBody = `
+    <div class="cateNew_da_1_0_0__service">
+      <h1 class="cateNew_da_1_0_0__title">${objLimit[0].cate}</h1>
+      <div class="cateNew_da_1_0_0__box">
+          ${htmlMain}
+          <div class="cateNew_da_1_0_0__content">${htmlSide}</div>
+      </div>
     </div>
-    </div>
-    `
-    // console.log(html2)
-    return html2;
-}
+    `;
 
-const renderThree = (obj) => {
-    let html = ""
-    let l = obj.length;
-    for(let i = 0; i < l; i++){
-        html += `${renderFour(obj[i])}`
-    }
-    console.log(html);
-}
+  return htmlBody;
+};
 
+const renderContentTab = (obj) => {
+  let html = "";
+  let objLenght = obj.length;
+  for (let i = 0; i < objLenght; i++) {
+    html += `${renderListNews(obj[i])}`;
+  }
+
+  document.getElementById('data_cate').innerHTML = html;
+};
+
+const elements = document.querySelectorAll(".tabNew_da_1_0_0__tablink");
+elements[0].classList.add('current');
+const lNew = renderList(dataNew, dataNew[0].cateGroup);
+const lCateGroup = listCateGroup(lNew);
+renderContentTab(lCateGroup);
