2700778eceef53ab26174c53010ef1311a966fe0
nguyenquocviet1002 <vietnguyen10022001@gmail.com>
2023-02-18 13:49:10 +0700

    update doctor_da_1_0_0

--- Home/doctor_da_1_0_0/js/doctor_da_1_0_0.js
+++ Home/doctor_da_1_0_0/js/doctor_da_1_0_0.js
@@ -1,5 +1,3 @@
-const url = "https://benhvienthammydonga.vn/";
-
 const dataDoctor = [
   {
     id: "1",
@@ -116,7 +114,7 @@ const showModal = (id, arr) => {
   });
   html = `
       <div class="modal popupdoctor_da_1_0_0" id="modal-doctor" style="display: flex">
-          <div class="modal-bg"></div>
+          <div class="modal-bg modal-bg--1"></div>
           <div class="modal-box animate-pop popupdoctor_da_1_0_0__box">
               <div class="popupdoctor_da_1_0_0__header">
                   <div class="modal-close popupdoctor_da_1_0_0__close">&times;</div>
@@ -133,22 +131,57 @@ const showModal = (id, arr) => {
               <div class="popupdoctor_da_1_0_0__body">
                 ${data[0].dataModal}
               </div>
-              <div class="popupdoctor_da_1_0_0__regist">
-                  <a href="#">
+              <div class="popupdoctor_da_1_0_0__regist" onclick="showModalRegister()">
+                  <a href="javascript:void(0)">
                       <div class="popupdoctor_da_1_0_0__coating">
                           <p>Đăng ký khám</p>
                       </div>
                       <div class="popupdoctor_da_1_0_0__iconReg">
-                          <img width="12" height="15" src="images/icon.png" alt="">
+                          <img width="12" height="15" src="https://benhvienthammydonga.vn/wp-content/themes/SCI_Theme/Module/Home/service_da_1_0_0/images/icon.png" alt="">
                       </div>
                   </a>
               </div>
           </div>
       </div>
      `;
-  document
-    .getElementsByClassName("doctor_da_1_0_0")[0]
-    .insertAdjacentHTML("beforeend", html);
+  document.getElementsByClassName("doctor_da_1_0_0")[0].insertAdjacentHTML("beforeend", html);
+};
+
+const showModalRegister = () => {
+  let html = `
+    <div class="modal doctor_da_1_0_0__register" id="modal-register" style="display: flex">
+      <div class="modal-bg modal-bg--1"></div>
+      <div class="modal-box animate-pop doctor_da_1_0_0__boxModal">
+          <div class="doctor_da_1_0_0__form">
+              <span>ĐẶT LỊCH TƯ VẤN CÙNG BÁC SĨ</span>
+              <article>
+                  <div class="doctor_da_1_0_0__formRegist ">
+                      <div class="input"><i class="icon-user"></i><input id="iname" name="iname" required=""
+                              placeholder="(*) Họ và tên" type="text"></div>
+                      <input type="hidden" id="gclid_field" name="gclid_field" value="">
+                      <input type="hidden" id="code_campaign" name="code_campaign" value="530039326">
+                      <input type="hidden" id="name_campaign" name="name_campaign"
+                          value="[Đông Á] SALE ĐẸP KỊP TẾT">
+                      <div class="input"><i class="icon-phone"></i><input id="imob" name="imob" required=""
+                              placeholder="(*) Số điện thoại" type="text"></div>
+                      <div class="input" style="display: none;"><i class="icon-mail-alt"></i><input id="iemail"
+                              style="display: none;" name="iemail" type="email" placeholder="Email:"></div>
+                      <input placeholder="Dịch vụ bạn muốn tư vấn *:" id="itext" name="itext"></input>
+                  </div>
+                  <div class="doctor_da_1_0_0__registModal" onclick="ants_send_contact(this)">
+                          <div class="doctor_da_1_0_0__coatingModal">
+                              <p>Gửi thông tin</p>
+                          </div>
+                          <div class="doctor_da_1_0_0__iconRegModal">
+                              <img width="15" height="12" src="https://benhvienthammydonga.vn/wp-content/themes/SCI_Theme/Module/Home/form_da_1_0_0/images/icon.png" alt="">
+                          </div>
+                  </div>
+              </article>
+          </div>
+      </div>
+    </div>
+     `;
+  document.getElementsByTagName("body")[0].insertAdjacentHTML("beforeend", html);
 };
 
 const renderData = (arr) => {
@@ -179,22 +212,28 @@ const renderData = (arr) => {
   return html;
 };
 
-document.getElementsByClassName("doctor_da_1_0_0__box")[0].innerHTML =
-  renderData(dataDoctorHome);
+document.getElementsByClassName("doctor_da_1_0_0__box")[0].innerHTML = renderData(dataDoctorHome);
 const popBtn = document.querySelectorAll(".doctor_da_1_0_0__item");
 for (let i = 0; i < popBtn.length; i++) {
   popBtn[i].addEventListener("click", () => {
     const idPop = popBtn[i].getAttribute("data-id");
     showModal(idPop, dataDoctorHome);
-    document
-      .querySelectorAll(".popupdoctor_da_1_0_0__close")[0]
-      .addEventListener("click", () => {
+    document.querySelectorAll(".popupdoctor_da_1_0_0__close")[0].addEventListener("click", () => {
         document.getElementById("modal-doctor").remove();
       });
-    window.onclick = (e) => {
-      if (e.target == document.getElementsByClassName("modal-bg")[0]) {
+    });
+  }
+  window.addEventListener("click", (e) => {
+    console.log()
+    if (e.target == document.getElementsByClassName("modal-bg--1")[0]) {
+      if(document.getElementById("modal-doctor")){
         document.getElementById("modal-doctor").remove();
       }
-    };
-  });
-}
+      if(document.getElementById("modal-register")){
+        document.getElementById("modal-register").remove();
+      }
+    }
+    else if (e.target == document.getElementsByClassName("modal-bg--1")[1]) {
+        document.getElementById("modal-register").remove();
+    }
+  })
